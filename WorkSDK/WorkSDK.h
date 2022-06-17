5bbe975c298a3a2c5f6c0f440002fa3ffbe18f5c
Long Nguyen <longnh264@gmail.com>
2022-06-17 14:07:14 +0700

    Test

---
+++ WorkSDK/WorkSDK.h
@@ -0,0 +1,18 @@
+//
+//  WorkSDK.h
+//  WorkSDK
+//
+//  Created by long on 17/06/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WorkSDK.
+FOUNDATION_EXPORT double WorkSDKVersionNumber;
+
+//! Project version string for WorkSDK.
+FOUNDATION_EXPORT const unsigned char WorkSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorkSDK/PublicHeader.h>
+
+
